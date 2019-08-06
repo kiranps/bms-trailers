aea3d65c19470c494962d39b912d9f1fc9166fb0
Kiran PS <pskirann@gmail.com>
2019-08-06 10:26:06 +0530

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        ghostwhite: "#f8f8ff",
+        black: {
+          "100": "#1a1a1a",
+          "200": "#2b2b2b",
+          "900": "#000000"
+        }
+      }
+    },
+    inset: {
+      "0": 0,
+      "12": "3rem"
+    }
+  },
+  variants: {
+    outline: ["focus", "responsive", "hover"]
+  },
+  plugins: []
+};
