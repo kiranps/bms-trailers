633abecd85fa02c1c0168daaac823f4038a86fd8
Kiran PS <pskirann@gmail.com>
2019-08-07 16:44:38 +0530

    add checkbox and hover open

---
+++ src/components/CheckBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const CheckBox = ({ active }) => (
+  <svg
+    style={{ display: "inline-block", width: "18px", height: "18px" }}
+    viewBox="0 0 24 24"
+  >
+    {active ? (
+      <path
+        fill="#38b2ac"
+        d="M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z"
+      />
+    ) : (
+      <path
+        fill="#ffffff85"
+        d="M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z"
+      />
+    )}
+  </svg>
+);
+
+CheckBox.defaultProps = {
+  active: false
+};
+
+export default CheckBox;
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
+import CheckBox from "/components/CheckBox";
 import * as R from "ramda";
 
 const Select = ({ placeholder, options, onChange }) => {
   const [isOpen, setOpen] = useState(false);
   const [selectedValues, setValues] = useState([]);
-
-  let toggleSelect = () => {
-    console.log(isOpen);
-    setOpen(!isOpen);
-  };
 
   let handleSelect = index => {
     const item = options[index];
@@ -21,43 +17,51 @@ const Select = ({ placeholder, options, onChange }) => {
   };
 
   return (
-    <div className="inline-block mx-2 align-top">
+    <div
+      className="inline-block mx-2 align-top"
+      onMouseOver={_ => setOpen(true)}
+      onMouseOut={_ => setOpen(false)}
+    >
       <div>
-        <div
-          className="border border-teal-500 px-2 rounded-sm text-xs w-32 overflow-hidden"
-          onClick={toggleSelect}
-        >
-          <div className="inline-block text-teal-500 overflow-none w-3/4">
+        <div className="border border-teal-500 rounded-sm text-xs w-32 py-1">
+          <div className="inline-block text-teal-500 overflow-none w-3/4 overflow-hidden px-2 align-middle">
             {R.isEmpty(selectedValues) ? placeholder : selectedValues.join(",")}
           </div>
-          <div className="inline-block w-1/4">
+          <div className="inline-block w-1/4 text-center">
             <svg
               viewBox="-1 -1 453.85 453.85"
               style={{ display: "inline-block", height: "12px", width: "12px" }}
             >
               <path
-                fill="#fff"
+                fill="#38b2ac"
                 d="M225.92 354.7c-8.1 0-16.2-3.09-22.37-9.26L9.27 151.16a31.64 31.64 0 1 1 44.75-44.75l171.9 171.91 171.9-171.9a31.64 31.64 0 0 1 44.75 44.74L248.3 345.45a31.55 31.55 0 0 1-22.37 9.26z"
               />
             </svg>
           </div>
         </div>
-        {isOpen && (
-          <div className="bg-black-200">
-            {options.map((x, i) => (
-              <div key={i} onClick={() => handleSelect(i)}>
-                <span>
-                  <input
-                    type="checkbox"
-                    readOnly
-                    checked={R.contains(x, selectedValues)}
-                  />
-                </span>
-                <span className="capitalize">{x}</span>
-              </div>
-            ))}
-          </div>
-        )}
+        <div className={"bg-black-200" + (isOpen ? " visible" : " hidden")}>
+          {options.map((x, i) => (
+            <div
+              key={i}
+              className="px-1 cursor-pointer"
+              onClick={() => handleSelect(i)}
+            >
+              <span className="align-middle">
+                <CheckBox active={R.contains(x, selectedValues)} />
+              </span>
+              <span
+                className={
+                  "capitalize text-xs align-middle px-1" +
+                  (selectedValues.includes(x)
+                    ? " text-teal-500"
+                    : " text-white")
+                }
+              >
+                {x}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
